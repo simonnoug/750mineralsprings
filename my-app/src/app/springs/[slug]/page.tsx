9c864e8b12c16b7fa975b71208190841599905f2
snougue <snougue@student.ethz.ch>
2025-06-05 01:52:02 +0200

    await

--- my-app/src/app/springs/[slug]/page.tsx
+++ my-app/src/app/springs/[slug]/page.tsx
@@ -2,6 +2,8 @@ import { getSpringBySlug } from "@/src/lib/sanity"
 import SpringPageContent from "@/src/app/springs/[slug]/SpringPageClient"
 
 export default async function SpringPage({ params }: { params: { slug: string } }) {
-  const spring = await getSpringBySlug(params.slug)
+  // on "await" maintenant params pour en extraire slug
+  const { slug } = await params
+  const spring = await getSpringBySlug(slug)
   return <SpringPageContent spring={spring} />
 }
